3654486387c57e7285dc9a4c7f0c3ffc7edee35d
Jeordane Batista <jeordanebatista@Jeordanes-MacBook-Pro.local>
2024-10-05 17:49:48 -0300

    feat: add queue listener

--- packages/aws-sqs/lib/index.ts
+++ packages/aws-sqs/lib/index.ts
@@ -1,6 +1,13 @@
 /* istanbul ignore file */
 
 import { MessageBatchPayload, MessageEnqueuer } from "@/producer";
-import { SQSProducerClient } from "./sqs-producer";
+import { SQSProducerClient } from "@/sqs-producer";
+import { QueueListener, QueueResolveWithInput } from "@/queue-listener";
 
-export { SQSProducerClient, MessageBatchPayload, MessageEnqueuer };
+export {
+  SQSProducerClient,
+  MessageBatchPayload,
+  MessageEnqueuer,
+  QueueListener,
+  QueueResolveWithInput,
+};
---
+++ packages/aws-sqs/lib/queue-listener.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Message } from "@aws-sdk/client-sqs";
+import { z } from "zod";
+
+export interface ResolveWithInput<
+  TSchema extends z.ZodType<any, any> = z.ZodType<any, any>
+> {
+  message: Message;
+  schema: TSchema;
+  resolveCallback: (message: z.infer<TSchema>) => Promise<void>;
+  payloadErrorCallback?: (
+    errors: z.typeToFlattenedError<z.infer<TSchema>, string>
+  ) => Promise<void>;
+}
+
+export abstract class QueueListener {
+  abstract handleMessage(message: Message): Promise<void>;
+
+  async resolveWith<TSchema extends z.ZodType<any, any>>(
+    args: ResolveWithInput<TSchema>
+  ): Promise<void> {
+    const { message, schema, resolveCallback, payloadErrorCallback } = args;
+    const parsedMessage = JSON.parse(message.Body as string);
+
+    if (typeof parsedMessage !== "object" || parsedMessage === null) {
+      throw new Error("Message must be a non-null object");
+    }
+
+    const result = schema.safeParse(parsedMessage);
+
+    if (!result.success) {
+      if (payloadErrorCallback) {
+        return await payloadErrorCallback(result.error.flatten());
+      }
+      throw new Error(JSON.stringify(result.error.flatten()));
+    }
+
+    return await resolveCallback(result.data);
+  }
+}
