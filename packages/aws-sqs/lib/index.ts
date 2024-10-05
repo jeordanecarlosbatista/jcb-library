7f04cddfff92d11b8eab21bbbbad3502e05d106d
Jeordane Batista <jeordanebatista@Jeordanes-MacBook-Pro.local>
2024-10-05 19:36:03 -0300

    feat: add queue listener interfaces

--- packages/aws-sqs/lib/index.ts
+++ packages/aws-sqs/lib/index.ts
@@ -2,5 +2,12 @@
 
 import { MessageBatchPayload, MessageEnqueuer } from "@/producer";
 import { SQSProducerClient } from "./sqs-producer";
+import { QueueListener, QueueResolveWithInput } from "./queue-listener";
 
-export { SQSProducerClient, MessageBatchPayload, MessageEnqueuer };
+export {
+  SQSProducerClient,
+  MessageBatchPayload,
+  MessageEnqueuer,
+  QueueListener,
+  QueueResolveWithInput,
+};
